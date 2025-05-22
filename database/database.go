79c32b80d65aeb9bad49bbb7c77db25d5c9db8c8
Abhishek2010dev <abhishekkumar2010dev@gmail.com>
2025-05-22 15:43:38 +0530

    migration sql code

--- database/database.go
+++ database/database.go
@@ -7,6 +7,17 @@ import (
 	_ "github.com/lib/pq"
 )
 
+var createShortenTableQuery = `
+CREATE TABLE IF NOT EXISTS shorten (
+	id SERIAL PRIMARY KEY,
+	url TEXT NOT NULL UNIQUE,
+	short_code TEXT NOT NULL UNIQUE,
+	access_count INT DEFAULT 0,
+	created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+`
+
 func New(url string) *sql.DB {
 	db, err := sql.Open("postgres", url)
 	if err != nil {
@@ -17,6 +28,10 @@ func New(url string) *sql.DB {
 	}
 
 	log.Printf("Successfully connected to postgresql")
+
+	if _, err := db.Exec(createShortenTableQuery); err != nil {
+		log.Fatalf("failed to create `shorten` table: %s", err)
+	}
 
 	return db
 }
