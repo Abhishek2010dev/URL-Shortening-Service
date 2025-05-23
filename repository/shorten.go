45eeab24a518347c05a581a2276e6e9d54e447d6
Abhishek2010dev <abhishekkumar2010dev@gmail.com>
2025-05-23 14:31:53 +0530

    update handler for reposioty

--- repository/shorten.go
+++ repository/shorten.go
@@ -6,24 +6,23 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/Abhishek2010dev/URL-Shortening-Service/dto"
 	"github.com/Abhishek2010dev/URL-Shortening-Service/model"
 	"github.com/jmoiron/sqlx"
 )
 
-var ErrShortCodeNotFound = errors.New("Shorten not found")
+var ErrShortCodeNotFound = errors.New("short_code not found")
 
-type CreateShortenPayload struct {
+type ShortenPayload struct {
 	Url       string `db:"url"`
 	ShortCode string `db:"short_code"`
 }
 
 type Shorten interface {
-	Create(ctx context.Context, payload CreateShortenPayload) (*model.Shorten, error)
+	Create(ctx context.Context, payload ShortenPayload) (*model.Shorten, error)
 	FindByShortCode(ctx context.Context, shortCode string) (*model.Shorten, error)
 	FindByShortCodeWithAccessCount(ctx context.Context, shortCode string) (*model.ShortenWithAccessCount, error)
 	Delete(ctx context.Context, shortCode string) error
-	Update(ctx context.Context, shortCode string, payload dto.ShortenPayload) (*model.Shorten, error)
+	Update(ctx context.Context, payload ShortenPayload) (*model.Shorten, error)
 	IncrementAccessCount(ctx context.Context, shortCode string) error
 }
 
@@ -35,7 +34,7 @@ func NewShorten(db *sqlx.DB) Shorten {
 	return &shortenRepo{db}
 }
 
-func (shortenrepo *shortenRepo) Create(ctx context.Context, payload CreateShortenPayload) (*model.Shorten, error) {
+func (shortenrepo *shortenRepo) Create(ctx context.Context, payload ShortenPayload) (*model.Shorten, error) {
 	query := `
 		INSERT INTO shorten (url, short_code) 
 	        VALUES (:url, :short_code)
@@ -64,10 +63,11 @@ func (shortenrepo *shortenRepo) FindByShortCode(ctx context.Context, shortCode s
 	stmt, err := shortenrepo.db.PreparexContext(ctx, query)
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
-			return nil, ErrShortenNotFound
+			return nil, ErrShortCodeNotFound
 		}
 		return nil, fmt.Errorf("failed to prepare query: %w", err)
 	}
+	defer stmt.Close()
 
 	var shorten model.Shorten
 	if err := stmt.GetContext(ctx, &shorten, shortCode); err != nil {
@@ -87,6 +87,7 @@ func (shortenrepo *shortenRepo) FindByShortCodeWithAccessCount(ctx context.Conte
 	if err != nil {
 		return nil, fmt.Errorf("failed to prepare query: %w", err)
 	}
+	defer stmt.Close()
 
 	var shortenWithAccessCount model.ShortenWithAccessCount
 	if err := stmt.GetContext(ctx, &shortenWithAccessCount, shortCode); err != nil {
@@ -106,8 +107,9 @@ func (shortenrepo *shortenRepo) Delete(ctx context.Context, shortCode string) er
 	if err != nil {
 		return fmt.Errorf("failed to prepare query: %w", err)
 	}
+	defer stmt.Close()
 
-	if _, err := stmt.Exec(shortCode); err != nil {
+	if _, err := stmt.ExecContext(ctx, shortCode); err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			return ErrShortCodeNotFound
 		}
@@ -117,9 +119,30 @@ func (shortenrepo *shortenRepo) Delete(ctx context.Context, shortCode string) er
 	return nil
 }
 
-func (shortenrepo *shortenRepo) Update(ctx context.Context, shortCode string, payload dto.ShortenPayload) (*model.Shorten, error) {
-	panic("not implemented") // TODO: Implement
+func (shortenrepo *shortenRepo) Update(ctx context.Context, payload ShortenPayload) (*model.Shorten, error) {
+	query := `
+		UPDATE shorten SET url = :url WHERE short_code = :short_code
+		RETURNING id, url, short_code, created_at, updated_at
+	`
+
+	stmt, err := shortenrepo.db.PrepareNamedContext(ctx, query)
+	if err != nil {
+		return nil, fmt.Errorf("failed to prepare query: %w", err)
+	}
+	defer stmt.Close()
+
+	var shorten model.Shorten
+	if err := stmt.GetContext(ctx, &shorten, payload); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrShortCodeNotFound
+		}
+		return nil, fmt.Errorf("failed to delete shorten (short_code: %v): %w", payload.ShortCode, err)
+	}
+
+	return &shorten, nil
 }
+
 func (shortenrepo *shortenRepo) IncrementAccessCount(ctx context.Context, shortCode string) error {
-	panic("not implemented") // TODO: Implement
+	query := "UPDATE shorten SET access_count = access_count + 1 WHERE short_code = $1"
+
 }
